6a26a8808f847e1e64b03babfa730fbaf43dbe5a
zhengxiaomei <1123056007@qq.com>
2023-07-27 18:14:33 +0800

    com

---
+++ packages/components/base-button/src/BaseButton.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind'
+import { FC, memo, useCallback, useMemo, useState } from 'react'
+import { Button } from 'antd'
+
+const View:FC = () => {
+  return <div>
+    <Button>tttttt</Button>
+  </div>
+}
+
+export default memo(View)
